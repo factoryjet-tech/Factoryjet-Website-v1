0990d8d74c9fe2c633c1ea81f79c567f4803d5a4
mr-rishikesh <mr.rishikesh2@gmail.com>
2026-04-16 01:42:43 +0530

    leeds page added

--- src/leeds-deployment/src/app/leeds/page.tsx
+++
@@ -1,8 +0,0 @@
-import { leedsMetadata } from "./metadata";
-import LeedsPage from "./LeedsPage";
-
-export const metadata = leedsMetadata;
-
-export default function Page() {
-  return <LeedsPage />;
-}
